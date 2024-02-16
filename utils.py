df3e5105fb97078d364175634b10455b2ef5d1d3
fernandoko3lle <fernandok3@al.insper.edu.br>
2024-02-15 22:29:41 -0300

    parte quatro

--- utils.py
+++ utils.py
@@ -12,9 +12,26 @@ def load_data(arquivo):
     x = Path("data")/arquivo
     with open(x, 'r') as file:
         return json.load(file)
-    
 
 def load_template(nome):
     with open(f"templates/{nome}", 'r') as file:
         return file.read()
     
+def adiciona_note(note):  
+    y = Path("data")/"notes,json"
+    if y.exists():
+        with open(y, 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
+    data.append(note)   
+    with open(y, 'w') as file:
+        json.dump(data, file) 
+
+def build_response(body="", code=200, reason="ok", headers=""):
+    if headers == "":
+        string = f"HTTP/1.1 {code} {reason}\n\n{body}"
+    else:
+        string = f"HTTP/1.1 {code} {reason}\n{headers}\n\n{body}"
+    return string.encode()
+
--- views.py
+++ views.py
@@ -1,11 +1,34 @@
-from utils import load_data, load_template
+from utils import load_data, load_template, adiciona_note, build_response
 
-def index():
+def index(request):
+    # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
+    if request.startswith('POST'):
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        params = {}
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+        for chave_valor in corpo:
+            lista = corpo.split('&')
+            lista_titulo = lista[0].split("=")
+            titulo = lista_titulo[1]
+            lista_detalhes = lista[1].split("=")
+            detalhes = lista_detalhes[1]
+            params['titulo'] = titulo
+            params['detalhes'] = detalhes
+        adiciona_note(params)
+
     note_template = load_template('components/note.html')
     notes_li = [
         note_template.format(title=dados['titulo'], details=dados['detalhes'])
         for dados in load_data('notes.json')
     ]
     notes = '\n'.join(notes_li)
+    body = load_template('index.html').format(notes=notes)
 
-    return load_template('index.html').format(notes=notes).encode()
+    return build_response(body)
